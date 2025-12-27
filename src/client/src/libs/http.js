da996eec1802e35a6b5f2eb4ef67d468327dbb23
thanhtien2208 <tiennt220805@gmail.com>
2025-12-27 15:06:06 +0700

    update: Combine domain to deploy

--- src/client/src/libs/http.js
+++ src/client/src/libs/http.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
+const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:3000" : "/";
+
 export const http = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
+  baseURL: BASE_URL,
   timeout: 10000,
 });
