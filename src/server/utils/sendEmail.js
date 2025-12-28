6aa88ea52df9e50f6b337da7d78ac09f4c7bf825
thanhtien2208 <tiennt220805@gmail.com>
2025-12-28 22:01:24 +0700

    fix: Update config for sendEmail

--- src/server/utils/sendEmail.js
+++ src/server/utils/sendEmail.js
@@ -1,22 +1,24 @@
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 
 const sendEmail = async (options) => {
-    //Tạo transporter
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: process.env.EMAIL_USERNAME,
-            pass: process.env.EMAIL_PASSWORD
-        }
-    });
-    const mailOptions = {
-        from: '"Sweetie Bakery" <noreply@sweetiebakery.com>',
-        to: options.email,
-        subject: options.subject,
-        html: options.message
-    };
+  //Tạo transporter
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+  const mailOptions = {
+    from: '"Sweetie Bakery" <noreply@sweetiebakery.com>',
+    to: options.email,
+    subject: options.subject,
+    html: options.message,
+  };
 
-    await transporter.sendMail(mailOptions);
+  await transporter.sendMail(mailOptions);
 };
 
 export default sendEmail;
